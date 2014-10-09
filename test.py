779e2f95887bf73b8736440512b1fe590cc15679
黄毅 <yi.codeplayer@gmail.com>
2014-10-09 19:40:17 +0800

    test memory image

--- test.py
+++ test.py
@@ -2,6 +2,10 @@ import sys
 from ccv import PY_CCV_IO_GRAY, DenseMatrix, ClassifierCascade, detect_objects
 
 matrix = DenseMatrix()
+
+#from PIL import Image
+#img = Image.open(sys.argv[1])
+#matrix.set_buf(img.tostring(), img.mode, img.size[0], img.size[1], PY_CCV_IO_GRAY)
 matrix.set_file(sys.argv[1], PY_CCV_IO_GRAY)
 
 cascade = ClassifierCascade()
