f9ceddd97fe70e8827a0ddc20020c5f31276f72f
黄毅 <yi.codeplayer@gmail.com>
2014-10-10 12:30:36 +0800

    build for python-for-android

--- setup.py
+++ setup.py
@@ -1,21 +1,37 @@
 #!/usr/bin/env python
 from distutils.core import setup
 from distutils.extension import Extension
-from Cython.Build import cythonize
 
-ext_modules = [ Extension("ccv", [
-                    "ccv.pyx",
-                    "deps/ccv/lib/ccv_bbf.c",
-                    "deps/ccv/lib/ccv_cache.c",
-                    "deps/ccv/lib/ccv_io.c",
-                    "deps/ccv/lib/ccv_memory.c",
-                    "deps/ccv/lib/ccv_resample.c",
-                    "deps/ccv/lib/ccv_util.c",
-                    "deps/ccv/lib/3rdparty/sha1/sha1.c",
-                ],
-                include_dirs=["deps/ccv/lib"],
-                define_macros=[("HAVE_LIBPNG", 1), ("HAVE_LIBJPEG", 1)],
-                libraries=["png", "jpeg"],
+try:
+    from Cython.Distutils import build_ext
+    have_cython = True
+except ImportError:
+    have_cython = False
+
+c_files = [
+    "deps/ccv/lib/ccv_bbf.c",
+    "deps/ccv/lib/ccv_cache.c",
+    "deps/ccv/lib/ccv_io.c",
+    "deps/ccv/lib/ccv_memory.c",
+    "deps/ccv/lib/ccv_resample.c",
+    "deps/ccv/lib/ccv_util.c",
+    "deps/ccv/lib/3rdparty/sha1/sha1.c",
+]
+
+if have_cython:
+    ccv_files = ["ccv.pyx"]
+    cmdclass = {'build_ext': build_ext}
+else:
+    ccv_files = ["ccv.c"]
+    cmdclass = {}
+
+ext_modules = [ Extension(
+                    "ccv",
+                    ccv_files + c_files,
+                    include_dirs=["deps/ccv/lib"],
+                    define_macros=[("HAVE_LIBPNG", 1), ("HAVE_LIBJPEG", 1)],
+                    libraries=["png", "jpeg"],
+                    extra_compile_args=['-ffast-math', '-fPIC'],
                 )
               ]
 
@@ -23,10 +39,11 @@ setup(
     name = 'ccv',
     version = '1.0.0',
     packages=['ccv'],
-    ext_modules = cythonize(ext_modules),
+    ext_modules = ext_modules,
     author='huangyi',
     author_email='yi.codeplayer@gmail.com',
     url = 'https://github.com/yihuang/pyccv',
     description = 'python binding of ccv library, written with cython.',
+    cmdclass=cmdclass,
     long_description='',
 )
